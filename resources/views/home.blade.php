fd223e5c94a29553bf3257de8403167f9e84dcc0
Cezar <cezarlantosgmail.com>
2022-03-04 17:05:54 +0100

    add style scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,24 +4,20 @@
 
 @section('content')
 
-    <main>
+    <div class="container">
 
-        <div class="container">
+        @foreach ($data as $value )
 
-            @foreach ($data as $value )
+            <div class="movie-card">
+                <h2>{{ $value['title'] }}</h2>    
+                <p>{{ $value['original_title'] }}</p>
+                <p>{{ $value['nationality'] }}</p>
+                <p>{{ $value['date'] }}</p>
+                <p>{{ $value['vote'] }}</p>
+            </div>
+            
+        @endforeach
 
-                <div class="movie-card">
-                    <h2>{{ $value['title'] }}</h2>    
-                    <p>{{ $value['original_title'] }}</p>
-                    <p>{{ $value['nationality'] }}</p>
-                    <p>{{ $value['date'] }}</p>
-                    <p>{{ $value['vote'] }}</p>
-                </div>
-                
-            @endforeach
-
-        </div>
-
-    </main>
+    </div>
 
 @endsection
